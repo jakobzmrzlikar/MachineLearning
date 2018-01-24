fb659075b6f6c1923b2907740111a5183c7741ff
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-01-24 09:37:20 +0100

    some tweaking of hyperparameters

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,19 @@ typedef std::vector<std::vector<double>> data;
 
 int main() {
 
-    double learning_rate = 0.1;
-    int epochs = 100;
+    double learning_rate = 0.00009;
+    int epochs = 10000;
 
-    LinearRegression example;
     DataLoader loader("data.csv");
     data training_data = loader.load();
+
+    LinearRegression example(training_data[0].size());
     std::vector<double> cost = example.train(training_data, learning_rate, epochs);
 
-    /*
-    for (int i=0; i<cost.size(); i++) {
-        std::cout << cost[i] << '\n';
-    }
-    */
 
-    for (int i=2012; i<2020; i++) {
-        std::cout << i << " " << example.h(i) << '\n';
+    for (int i=0; i<cost.size(); i++) {
+        std::cout << "cost: " << cost[i] << '\n';
     }
 
     return 0;
 }
-
