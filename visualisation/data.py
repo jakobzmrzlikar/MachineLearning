b02156e768193a526131370435d3cb090d3fb25d
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-02-01 20:44:11 +0100

    added some nicer graphs

--- visualisation/data.py
+++ visualisation/data.py
@@ -5,8 +5,14 @@ cost = np.genfromtxt('../data/cost.csv', delimiter='\n', skip_header=0,
                      skip_footer=0)
 data = np.genfromtxt('../data/train.csv', delimiter=',', skip_header=0,
                      skip_footer=0, names=['x', 'y'])
-x=data['x']
-y=data['y']
+weights = np.genfromtxt('../data/save.csv', delimiter=',', skip_header=0,
+                     skip_footer=0)
+
+x_data=data['x']
+y_data=data['y']
+
+x = [min(x_data), max(x_data)]
+y = [weights[0]+x[0]*weights[1], weights[0]+x[1]*weights[1]]
 
 
 plt.figure("Linear regression")
@@ -14,12 +20,13 @@ plt.figure("Linear regression")
 plt.subplot(211)
 plt.xlabel("x")
 plt.ylabel("y")
-plt.scatter(x,y)
+plt.scatter(x_data,y_data)
+plt.plot(x, y, 'r',linewidth=2)
 
 
 plt.subplot(212)
 plt.xlabel("epochs")
 plt.ylabel("cost")
-plt.plot(cost)
+plt.plot(cost,linewidth=2.5)
 
 plt.show()
