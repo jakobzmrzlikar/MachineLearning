fd36bcd7f0f1446bdb669cd12070da03eaf5465d
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-02-01 16:57:05 +0100

    some code refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,34 @@ typedef std::vector<std::vector<double>> data;
 
 int main() {
 
-    double learning_rate = 0.00009;
-    int epochs = 10000;
+    double learning_rate = 0.0001;
+    int epochs = 100;
 
-    DataLoader loader("data.csv");
+    DataLoader loader("train.csv");
     data training_data = loader.load();
+    DataLoader test("test.csv");
+    data test_data = test.load();
 
     LinearRegression example(training_data[0].size());
-    std::vector<double> cost = example.train(training_data, learning_rate, epochs);
+    std::vector<double> training_cost = example.train(training_data, learning_rate, epochs);
 
 
-    for (int i=0; i<cost.size(); i++) {
-        std::cout << "cost: " << cost[i] << '\n';
+    double test_cost = 0.0;
+    double a = 0;
+    double y = 0;
+    for (int i=0; i<test_data.size(); i++) {
+        a = example.h(test_data[i][0]);
+        y = test_data[i][1];
+        test_cost += QuadraticCost::cost(a, y);
+        //std::cout << a << " " << y << '\n';
     }
+    test_cost = test_cost/test_data.size();
+
+    for (int i=0; i<training_cost.size(); i++) {
+      std::cout << training_cost[i] << '\n';
+    }
+
+    std::cout << test_cost << '\n';
 
     return 0;
 }
