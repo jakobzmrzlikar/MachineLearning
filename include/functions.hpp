906efd7640ae063e17a0c76d3137f97942702dba
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-02-05 21:49:31 +0100

    added Cross-Entropy cost function

--- include/functions.hpp
+++ include/functions.hpp
@@ -14,10 +14,23 @@ public:
 
 };
 
+class CrossEntropyCost {
+  static double cost(double a, double y);
+  static double error_b(double a, double y);
+  static double error_w(double a, double y, double x);
+
+};
+
 class Standardization {
 public:
     static data normalize(data training_data);
     static data scale(data training_data);
+};
+
+class Functions {
+public:
+    static double sigmoid(double z);
+    static double sigmoid_prime(double z);
 };
 
 #endif /* FUNCTIONS_H */
