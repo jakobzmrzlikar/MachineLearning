37b356d4936e25596ebcf5d77e0be968caecbd61
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-02-06 21:14:35 +0100

    made linear regression compatible with multiple input variables

--- src/LinearRegression.cpp
+++ src/LinearRegression.cpp
@@ -15,31 +15,34 @@ LinearRegression::LinearRegression(int features){
 
 }
 
-double LinearRegression::h(double x) {
-    return (w[0] + w[1] * x);
+double LinearRegression::h(std::vector<double> x) {
+    double result = w[0];
+    for (int i=1; i<w.size(); i++) {
+        result+=w[i] * x[i-1];
+    }
+    return result;
 }
 
 std::vector<double> LinearRegression::train(data training_data, double learning_rate, int epochs) {
 
     int m = training_data.size();
 
-    std::vector<double> costs;
-    costs.push_back(cost(training_data, m));
-
+    std::vector<double> training_cost;
+    training_cost.push_back(cost(training_data, m));
     for (int i=0; i<epochs; i++){
         gradient_descent(training_data, learning_rate, m);
-        costs.push_back(cost(training_data, m));
+        training_cost.push_back(cost(training_data, m));
     }
 
-    return costs;
+    return training_cost;
 
 }
 
 double LinearRegression::cost(data training_data, int m) {
     double cost = 0.0;
     for (int i=0; i<m; i++) {
-        double x = training_data[i][0];
-        double y = training_data[i][1];
+        std::vector<double> x(training_data[i].begin(), training_data[i].end()-1);
+        double y = training_data[i].back();
         double a = h(x);
         cost += QuadraticCost::cost(a, y);
     }
@@ -52,16 +55,18 @@ void LinearRegression::gradient_descent(data training_data, double learning_rate
 
     // compute gradients
     for (int i=0; i<m; i++) {
-        double x = training_data[i][0];
-        double y = training_data[i][1];
+        std::vector<double> x(training_data[i].begin(), training_data[i].end()-1);
+        double y = training_data[i].back();
         double a = h(x);
         error[0] += QuadraticCost::error_b(a, y);
-        error[1] += QuadraticCost::error_w(a, y, x);
+        for (int j=1; j<training_data[0].size(); j++) {
+          error[j] += QuadraticCost::error_w(a, y, x[j]);
+        }
+
     }
 
-
     // update parameters with gradients
-    for (int i=0; i<this->w.size(); i++) {
+    for (int i=0; i<w.size(); i++) {
         w[i] -= learning_rate / m * error[i];
     }
 }
@@ -69,9 +74,9 @@ void LinearRegression::gradient_descent(data training_data, double learning_rate
 void LinearRegression::save(std::string filename) {
     std::string name = "../data/" + filename;
     std::ofstream file(name);
-    file << this->w[0];
-    for (int i=1; i<this->w.size(); i++) {
-      file << "," << this->w[i];
+    file << w[0];
+    for (int i=1; i<w.size(); i++) {
+      file << "," << w[i];
     }
     file << std::endl;
     std::cout << "Model saved to path: " << name << '\n';
@@ -81,14 +86,14 @@ void LinearRegression::load(std::string filename) {
     std::string name = "../data/" + filename;
     std::ifstream file(name);
     std::string line;
-    this->w.clear();
+    w.clear();
 
     while(getline(file,line)) {
         std::stringstream linestream(line);
         std::string value;
 
         while(getline(linestream,value,',')) {
-            this->w.push_back(atof(value.c_str()));
+            w.push_back(atof(value.c_str()));
         }
 
     }
