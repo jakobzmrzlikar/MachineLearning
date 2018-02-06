abb20124691274c6e666448edc25c5dfe406966a
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-02-06 21:16:00 +0100

    multivariate linear regression test

--- src/main.cpp
+++ src/main.cpp
@@ -8,37 +8,31 @@ typedef std::vector<std::vector<double>> data;
 
 int main() {
 
-    double learning_rate = 0.0001;
+    // Hyperparameters:
+    double learning_rate = 0.000001;
     int epochs = 100;
 
-    DataLoader loader("train.csv");
+    DataLoader loader("train2.csv");
     data training_data = loader.load();
+
     DataLoader test("test.csv");
     data test_data = test.load();
 
     LinearRegression example(training_data[0].size());
     std::vector<double> training_cost = example.train(training_data, learning_rate, epochs);
-    example.save("save.csv");
+    //example.save("save.csv");
+
+    example.load("save.csv");
+
+    double test_cost = example.cost(test_data, test_data.size());
 
 
-    double test_cost = 0.0;
-    double a = 0;
-    double y = 0;
-    for (int i=0; i<test_data.size(); i++) {
-        a = example.h(test_data[i][0]);
-        y = test_data[i][1];
-        test_cost += QuadraticCost::cost(a, y);
-        //std::cout << a << " " << y << std::endl;
-    }
-    test_cost = test_cost/test_data.size();
-
-    /*
     for (int i=0; i<training_cost.size(); i++) {
         std::cout << training_cost[i] << std::endl;
     }
 
-    std::cout << test_cost << std::endl;
-    */
+    //std::cout << test_cost << std::endl;
+
 
     return 0;
 }
