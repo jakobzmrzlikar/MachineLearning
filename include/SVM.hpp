b6e7d406a8720a1a7f22f53718907a623760e15f
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-02-15 22:08:28 +0100

    started working on SVM

---
+++ include/SVM.hpp
@@ -0,0 +1,25 @@
+#ifndef SVM_H
+#define SVM_H
+
+// training_data vector consisting of pairs (x,y)
+typedef std::vector<std::vector<double>> data;
+
+class SVM {
+
+public:
+    
+    // vector of weights
+    std::vector<double> w;
+
+    SVM();
+
+    // call function
+    double h(std::vector<double>& x);
+    // The above function actually returns -1 or 1, double used for consistency
+
+
+
+
+};
+
+#endif // SVM_H
