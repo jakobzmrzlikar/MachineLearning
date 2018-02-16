2b4c3776e2ab56fee1cb5aa9c9d1a9f0b36b291d
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-02-16 08:30:21 +0100

    fixed a small naming error

--- src/SVM.cpp
+++ src/SVM.cpp
@@ -6,8 +6,8 @@ typedef std::vector<std::vector<double>> data;
 
 SVM::SVM() {}
 
-double LinearRegression::h(std::vector<double>& x) {
-    double result = Functions::dot_product(w, x);
+double SVM::h(std::vector<double>& x) {
+    double result = dot_product(w, x);
     if (result >= 1) {
         return 1;
     } else if (result <= -1) {
