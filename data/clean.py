8e5d4b04ad81b071f7be29c77bf35837070c665a
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-03-04 15:38:03 +0100

    conducted new experiments

--- data/clean.py
+++ data/clean.py
@@ -17,6 +17,9 @@
 #         line[-1] = "0"
 #     print(",".join(line))
 
-for i in range(1000):
-    line = input().split()
-    print(",".join(line))
+# for i in range(5875):
+#     line = input().split(",")
+#     line = line[4:]
+#     if line[-1] != "0":
+#         line[-1] = str(math.log(float(line[-1])))
+#     print(",".join(line))
